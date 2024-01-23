82b015aadddb5b2eb01da1ef362cd0cb4431633b
Vicdem989 <vicdemhoy@hotmail.com>
2024-01-23 11:41:39 +0100

    added needed .cs file as well :)

---
+++ task5.cs
@@ -0,0 +1,55 @@
+// Your program should start at this line.
+/*
+Example:
+Move();
+Move();
+Move();
+Turn();
+Move();
+Move();
+Turn();
+Turn();
+Turn();
+Move();
+*/
+
+///While not at goal
+///check if cell infront is open
+/// if check == true => go
+/// else turn right
+/// if check still false, turn other way twice (left)
+/// if still false, go back until no open space
+
+
+
+#region Basic functions
+// These functions are just her to make your intelisense work. 
+// They only have a conceptual function.
+
+void Move()
+{
+    // Moves the car 1 cell in the direction it is heading. 
+}
+
+void Turn()
+{
+    // Turns the car 90 deg clockwise.
+}
+
+bool Peek()
+{
+    // Returns true if the next cell is open, otherwise false.
+    return true; // Just a placeholder value. 
+}
+
+bool AtGoal()
+{
+    // Returns true if the current cell is the goal cell.
+    return true; // just a placholder
+}
+
+#endregion
+
+#region Helper functions
+
+#end region
