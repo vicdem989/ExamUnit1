3f159b1edec5f5e4961f100b9fffdf879cd2f575
Vicdem989 <vicdemhoy@hotmail.com>
2024-01-23 13:54:29 +0100

    added logic to check front and turn right if needed

---
+++ task2.cs
@@ -0,0 +1,47 @@
+
+
+///While not at goal
+///check if cell infront is open
+/// if not, turn right and go
+
+while(!AtGoal) {
+    if(Peek) {
+        Move();
+    } else {
+        Turn();
+    }
+}
+
+
+
+#region Basic functions
+// These functions are just her to make your intelisense work. 
+// They only have a conceptual function.
+
+void Move()
+{
+    // Moves the car 1 cell in the direction it is heading. 
+}
+
+void Turn()
+{
+    // Turns the car 90 deg clockwise.
+}
+
+bool Peek()
+{
+    // Returns true if the next cell is open, otherwise false.
+    return true; // Just a placeholder value. 
+}
+
+bool AtGoal()
+{
+    // Returns true if the current cell is the goal cell.
+    return true; // just a placholder
+}
+
+#endregion
+
+#region Helper functions
+
+#end region
